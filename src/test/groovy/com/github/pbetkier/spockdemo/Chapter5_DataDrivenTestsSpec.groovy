5b7b653122f85c9a6758ff625fd12cb5343893a0
Piotr Betkier <pbetkier@gmail.com>
2014-06-22 23:16:34 +0200

    added data driven support spec.

---
+++ src/test/groovy/com/github/pbetkier/spockdemo/Chapter5_DataDrivenTestsSpec.groovy
@@ -0,0 +1,49 @@
+package com.github.pbetkier.spockdemo
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class Chapter5_DataDrivenTestsSpec extends Specification {
+
+    def "should allow achieving lightweight tests parameterization"() {
+        expect:
+        Math.max(a, b) == result
+
+        where:
+        a | b | result
+        1 | 2 | 2
+        2 | 1 | 2
+        0 | 0 | 0
+    }
+
+    @Unroll
+    def "max between #a and #b should be #result"() {
+        expect:
+        Math.max(a, b) == result
+
+        where:
+        a | b | result
+        1 | 2 | 2
+        2 | 1 | 2
+        0 | 0 | 0
+    }
+
+    def "should allow for another syntax for tests parameterization"() {
+        expect:
+        name.size() == size
+
+        where:
+        name << ["John", "William", "Sue"]
+        size << [4, 7, 3]
+    }
+
+    def "... and another one..."() {
+        expect:
+        name.size() == size
+
+        where:
+        [name, size] << [["John", 4], ["William", 7], ["Sue", 3]]
+        // [name, size] << sql.rows("select name, size from names")
+    }
+
+}
